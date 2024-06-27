ff14655349677602b2e88b7457ddcb3cf81f667b
jxkimathi <judescriva8@gmail.com>
2024-06-27 22:34:57 +0300

    correction

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""Pascal's Triangle"""
+
+
 def pascal_triangle(n):
     """Generates a Pascal's Triangle up to the nth row
         Returns an empty list when n <= 0
@@ -11,9 +15,9 @@ def pascal_triangle(n):
     # Iterate through rows starting from the 2nd row
     for i in range(1, n + 1):
         level = []
-        C = 1
+        first = 1
         for j in range(1, i + 1):
-            level.append(C)
-            C = C * (i - j) // j
+            level.append(first)
+            first = first * (i - j) // j
         triangle.append(level)
     return triangle
