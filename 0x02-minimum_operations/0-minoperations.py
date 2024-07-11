891615f10c09ce2aa8741c937056372d385319f1
jxkimathi <judescriva8@gmail.com>
2024-07-11 17:16:16 +0300

    correction on the indentation, lines 20-31

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,32 +4,31 @@
 
 def minOperations(n):
     """Calculates the fewest number of operations needed"""
-    copied_chars = 0 # Number of H
-    pasted_chars = 1 # Number of hars
+    copied_chars = 0  # Number of H
+    pasted_chars = 1  # Number of hars
     counter = 0
 
     while pasted_chars < n:
-        if copied_chars == 0: # If no chars are copied
+        if copied_chars == 0:  # If no chars are copied
             pasted_chars = copied_chars
             counter += 1
 
-        if pasted_chars == 1: # If nothing has been pasted
+        if pasted_chars == 1:  # If nothing has been pasted
             pasted_chars += copied_chars
             counter += 1
             continue
+        remaining_chars = n - pasted_chars  # Remainig chars to paste
 
-    remaining_chars = n - pasted_chars # Remainig chars to paste
+        if remaining_chars < copied_chars:
+            return 0
 
-    if remaining_chars < copied_chars:
-        return 0
-
-    if remaining_chars % pasted_chars != 0:
-        pasted_chars += copied_chars
-        counter += 1
-    else:
-        copied_chars = pasted_chars
-        pasted_chars += copied_chars
-        counter += 2
+        if remaining_chars % pasted_chars != 0:
+            pasted_chars += copied_chars
+            counter += 1
+        else:
+            copied_chars = pasted_chars
+            pasted_chars += copied_chars
+            counter += 2
 
     if pasted_chars == n:
         return counter
